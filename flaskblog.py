38c8c09d75fb03e09543cd7c14d19ba289c907dd
Tyler Lewis <tylerlewis182@gmail.com>
2019-10-24 12:43:50 -0700

    finished forms and validation

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect, flash
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'be710bf3f267988939ff1c647daf9020' # from python interp: import secrets; secrets.token_hex(16)
 
 
 posts = [
@@ -29,6 +31,28 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route("/register", methods=['GET', 'POST']) # allowed methods
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
 
 
 
